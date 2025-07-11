d39b3d31b23678e0bfe6cfd60ac2a93ae837b431
redDwarf03 <sylvain@archethic.net>
2025-07-11 15:35:23 +0200

    Add Unit tests

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
+    verbose: true,
+    setupFiles: ['./tests/setup.ts'],
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.ts$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+}; 
