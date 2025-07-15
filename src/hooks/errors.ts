52270acc98f55d21b77000d037e10537cd3bdc0f
redDwarf03 <sylvain@archethic.net>
2025-07-15 22:48:54 +0200

    feat: :sparkles: Add Redis service to Docker Compose for improved caching and real-time data handling; update environment configurations for production and test environments; refactor WebSocket server initialization to occur after Redis connection

--- src/hooks/errors.ts
+++ src/hooks/errors.ts
@@ -1,6 +1,7 @@
 const catchErrors = () => {
   process.on("uncaughtException", async function (err) {
     console.log(`Caught exception at ${new Date()}: ` + err);
+    console.log("Stack trace:", err.stack);
   });
 };
 
